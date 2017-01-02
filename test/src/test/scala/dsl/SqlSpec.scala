f3786442029399e4a41a9d1c5b923e75e06fff52
Mark <mark.de.jong@lunatech.com>
2017-01-02 12:44:02 +0100

    Added as spec

--- test/src/test/scala/dsl/SqlSpec.scala
+++ test/src/test/scala/dsl/SqlSpec.scala
@@ -1,8 +1,24 @@
 package dsl
 
-/**
-  * Test spec for Sql DSL
-  */
-class SqlSpec {
+import org.scalatest._
+import oriented._
+import oriented.syntax._
 
+class SqlSpec extends Suite with WordSpecLike with Matchers with BeforeAndAfter  {
+
+  implicit val orientClient = InMemoryClient("SqlSpec")
+
+  case class Person(name: String, age: Int)
+
+  "SqlSpec" should {
+    "be to use as cast" in {
+
+      val prg = for {
+        _ <- orientClient.addVertex(Person("Mark", 1337))
+        age <- sql"SELECT age FROM Person LIMIT 1".as[Int]("age")
+      } yield age
+
+      prg.runGraphUnsafe(enableTransactions = false) shouldBe 1337
+    }
+  }
 }
